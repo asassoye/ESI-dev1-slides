1b806b845cd4891cd0664bba00b81de898d2c856
Andrew Sassoye <andrew@sassoye.be>
2019-11-04 12:35:18 +0100

    declaration typo

--- contents/declaration-et-creation/java/Declaration.java
+++ contents/declaration-et-creation/java/Declaration.java
@@ -1,7 +1,7 @@
 public class Declaration {
     public static void main(String[] args) {
         int[] array;
-        double[] dubbleArray;
+        double[] doubleArray;
         int[] a1, a2;
         int a, b, c[];
         Point[] points;
