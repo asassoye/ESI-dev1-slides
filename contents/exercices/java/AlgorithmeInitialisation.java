2844c162881fa7a3ca42165469763fe2c937b63c
Andrew Sassoye <andrew@sassoye.be>
2019-11-08 11:40:57 +0100

    Solution Syllabus Exercice 97

---
+++ contents/exercices/java/AlgorithmeInitialisation.java
@@ -0,0 +1,8 @@
+public class AlgorithmeInitialisation {
+    public static void main(String[] args) {
+        int[] tableau = new int[100];
+        for (int i = 0; i < tableau.length; ++i) {
+            tableau[i] = i + 1;
+        }
+    }
+}
