57045d214f7a33f1ccdcee86f21c944c8b2225ee
Andrew SASSOYE <andrew@sassoye.be>
2019-11-10 17:15:01 +0100

    Solution Syllabus Exercice 99

---
+++ contents/exercices/java/SyllabusExercice99.java
@@ -0,0 +1,9 @@
+public class SyllabusExercice99 {
+    public int[] inverseLesElementsNegatifs(int[] tableau);
+    public int nbElementsNegatifs(int[] tableau);
+    public boolean contientAuMoinsUnNbNegatif(int[] tableau);
+    public boolean contientChaine(String[] tableau, String chaine);
+    public boolean contientDoublon(String[] tableau);
+    public int[] nombresPremiers(int n);
+    public boolean[] nombreEstPositif(int[] tableau);
+}
