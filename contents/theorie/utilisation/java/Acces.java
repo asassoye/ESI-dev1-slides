2a7c6c8200f0702bbe90a12b253553cce193483c
Andrew SASSOYE <andrew@sassoye.be>
2019-11-11 14:46:23 +0100

    Utilisation: Accès individuel aux éléments d'un tableau

---
+++ contents/theorie/utilisation/java/Acces.java
@@ -0,0 +1,8 @@
+public class Acces {
+    public static void main(String[] args) {
+        int[] tableau = new int[5];
+        tableau[0] = 15;
+        System.out.printf("La valeur dans le tableau a l'indice 0 est %d", tableau[0]);
+        tableau[15] = 10;
+    }
+}
