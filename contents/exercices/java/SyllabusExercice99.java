161137ca600dee87f325ccbab46e273c685dddd1
Andrew SASSOYE <andrew@sassoye.be>
2019-11-11 18:39:32 +0100

    Syllabus ex 9 fix @YoungFrog

--- contents/exercices/java/SyllabusExercice99.java
+++ contents/exercices/java/SyllabusExercice99.java
@@ -1,9 +1,9 @@
 public class SyllabusExercice99 {
-    public int[] inverseLesElementsNegatifs(int[] tableau);
-    public int nbElementsNegatifs(int[] tableau);
-    public boolean contientAuMoinsUnNbNegatif(int[] tableau);
-    public boolean contientChaine(String[] tableau, String chaine);
-    public boolean contientDoublon(String[] tableau);
-    public int[] nombresPremiers(int n);
-    public boolean[] nombreEstPositif(int[] tableau);
+    public static void[] inverseLesElementsNegatifs(int[] tableau);
+    public static int nbElementsNegatifs(int[] tableau);
+    public static boolean contientAuMoinsUnNbNegatif(int[] tableau);
+    public static boolean contientChaine(String[] tableau, String chaine);
+    public static boolean contientDoublon(String[] tableau);
+    public static int[] nombresPremiers(int n);
+    public static boolean[] nombreEstPositif(int[] tableau);
 }
